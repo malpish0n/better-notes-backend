73f6b8a20cfbe0f7526a90518d7a2cd7ba214f15
malpish0n <akasztelan617@gmail.com>
2025-04-23 23:53:07 +0200

    added note object

---
+++ src/main/java/dev/malpish0n/model/Note.java
@@ -0,0 +1,41 @@
+package dev.malpish0n.model;
+
+public class Note {
+    
+    public Long id;
+    public String title;
+    public String content;
+
+
+    public Note(Long id, String title, String content) {
+
+        this.id = id;
+        this.title = title;
+        this.content = content;
+        
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    } 
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
