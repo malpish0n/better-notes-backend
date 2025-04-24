24b352f9f77f326f4c27ee25f2e9db2e65e91827
malpish0n <akasztelan617@gmail.com>
2025-04-24 18:00:23 +0200

    Updated NoteService

--- src/main/java/dev/malpish0n/service/NoteService.java
+++ src/main/java/dev/malpish0n/service/NoteService.java
@@ -1,8 +1,48 @@
 package dev.malpish0n.service;
-
 import org.springframework.stereotype.Service;
+import dev.malpish0n.model.Note;
+import dev.malpish0n.repository.NoteRepository;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class NoteService {
     
+    private final NoteRepository noteRepository;
+
+    public NoteService(NoteRepository noteRepository) {
+        this.noteRepository = noteRepository;
+    }
+
+    public List<Note> getAllNotes() {
+        return noteRepository.findAll();
+    }
+
+    public Optional<Note> getNoteById(Long id) {
+        return noteRepository.findById(id);
+    }
+
+    public Note createNote(Note note) {
+        return noteRepository.save(note);
+    }
+
+    public void deleteNoteById(Long id) {
+        noteRepository.deleteById(id);
+    }
+
+    public Note updateNote(Long id, Note updatedNote) {
+        return noteRepository.findById(id)
+                .map(existingNote -> {
+                    existingNote.setTitle(updatedNote.getTitle());
+                    existingNote.setContent(updatedNote.getContent());
+                    return noteRepository.save(existingNote);
+                })
+                .orElseGet(() -> {
+                    updatedNote.setId(id);
+                    return noteRepository.save(updatedNote);
+                });
+    }
+}
+
 }
